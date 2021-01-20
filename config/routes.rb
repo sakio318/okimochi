61f0a3ab3722bc6c2e84b6e1ff4d6f69101ac61a
sakio318 <saki1803terashima@gmail.com>
2021-01-20 11:24:18 +0000

    add_heder

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,16 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'about' => 'homes#about'
   devise_for :users
-  
+
   resources :items do
-    resources:favorites, only:[:create,:destroy]
+    resources:favorites, only:[:index,:create,:destroy]
     resources:comments, only:[:create,:destroy]
     resources:reviews, only:[:create,:destroy]
   end
-  
+
   resources :genres, only:[:create, :destroy]
   resources :images, only:[:create, :update, :destroy]
-  
-  
+
+
 
 end

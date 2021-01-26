b397e4dac82ae29766dd93b1d2156f29e364adef
sakio318 <saki1803terashima@gmail.com>
2021-01-26 03:47:35 +0000

    add_ranking

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,5 +15,3 @@
 //= require bootstrap-sprockets
 //= require rails-ujs
 //= require activestorage
-//= require turbolinks
-//= require_tree .
--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,2 +1,6 @@
 class HomesController < ApplicationController
+  def top
+    @favorite = Favorite.all
+    @items = Item.find(@favorite.group(:item_id).order('count(item_id)desc').limit(3).pluck(:item_id))
+  end
 end

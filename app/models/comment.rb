7ce37efdbe5048b051c14c7cbb3cf28973e47053
sakio318 <saki1803terashima@gmail.com>
2021-02-01 16:34:18 +0000

    update_serch

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :item
+  validates :rate, numericality:{
+    less_than_or_equal_to: 5,
+    greater_than_or_equal_to: 1
+  },presence: true
 end

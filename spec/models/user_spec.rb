071b456142ff2854958a48d4d9699cbf40cc4a57
sakio318 <saki1803terashima@gmail.com>
2021-02-07 15:39:33 +0000

    update_valide_about

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,10 +2,12 @@
 
 require 'rails_helper'
 
-RSpec.describe User,"ユーザーに関するテスト", type: :model do
+RSpec.describe User,"userモデルに関するテスト", type: :model do
   describe'実際に保存してみる' do
     it "有効な新規登録の場合は保存されるか" do
       expect(FactoryBot.build(:user)).to be_valid
     end
   end
+  describe'バリデーションのテスト' do
+    it
 end
